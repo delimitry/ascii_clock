30ff4879c1c22095b1c612078d859fd718e0d9eb
Dmitry Alimov <dvalimov@gmail.com>
2016-09-22 16:39:29 +0300

    Added Python 3 support

--- clock.py
+++ clock.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#-*- coding: utf8 -*-
+#-*- coding: utf-8 -*-
 #-----------------------------------------------------------------------
 # Author: delimitry
 #-----------------------------------------------------------------------
@@ -22,7 +22,7 @@ def draw_second_hand(ascii_canvas, seconds, length, fill_char):
     y0 = int(math.ceil(ascii_canvas.lines / 2.0))
     x1 = x0 + int(math.cos((seconds + 45) * 6 * math.pi / 180) * length * x_scale_ratio)
     y1 = y0 + int(math.sin((seconds + 45) * 6 * math.pi / 180) * length)
-    ascii_canvas.add_line(x0, y0, x1, y1, fill_char=fill_char)
+    ascii_canvas.add_line(int(x0), int(y0), int(x1), int(y1), fill_char=fill_char)
 
 
 def draw_minute_hand(ascii_canvas, minutes, length, fill_char):
@@ -33,7 +33,7 @@ def draw_minute_hand(ascii_canvas, minutes, length, fill_char):
     y0 = int(math.ceil(ascii_canvas.lines / 2.0))
     x1 = x0 + int(math.cos((minutes + 45) * 6 * math.pi / 180) * length * x_scale_ratio)
     y1 = y0 + int(math.sin((minutes + 45) * 6 * math.pi / 180) * length)
-    ascii_canvas.add_line(x0, y0, x1, y1, fill_char=fill_char)
+    ascii_canvas.add_line(int(x0), int(y0), int(x1), int(y1), fill_char=fill_char)
 
 
 def draw_hour_hand(ascii_canvas, hours, minutes, length, fill_char):
@@ -45,23 +45,23 @@ def draw_hour_hand(ascii_canvas, hours, minutes, length, fill_char):
     total_hours = hours + minutes / 60.0
     x1 = x0 + int(math.cos((total_hours + 45) * 30 * math.pi / 180) * length * x_scale_ratio)
     y1 = y0 + int(math.sin((total_hours + 45) * 30 * math.pi / 180) * length)
-    ascii_canvas.add_line(x0, y0, x1, y1, fill_char=fill_char)
+    ascii_canvas.add_line(int(x0), int(y0), int(x1), int(y1), fill_char=fill_char)
 
 
 def draw_clock_face(ascii_canvas, radius, mark_char):
     """
     Draw clock face with hour and minute marks
     """
-    x0 = ascii_canvas.cols / 2
-    y0 = ascii_canvas.lines / 2
+    x0 = ascii_canvas.cols // 2
+    y0 = ascii_canvas.lines // 2
     # draw marks first
-    for mark in xrange(1, 12 * 5 + 1):
+    for mark in range(1, 12 * 5 + 1):
         x1 = x0 + int(math.cos((mark + 45) * 6 * math.pi / 180) * radius * x_scale_ratio)
         y1 = y0 + int(math.sin((mark + 45) * 6 * math.pi / 180) * radius)
         if mark % 5 != 0:
             ascii_canvas.add_text(x1, y1, mark_char)
     # start from 1 because at 0 index - 12 hour
-    for mark in xrange(1, 12 + 1):
+    for mark in range(1, 12 + 1):
         x1 = x0 + int(math.cos((mark + 45) * 30 * math.pi / 180) * radius * x_scale_ratio)
         y1 = y0 + int(math.sin((mark + 45) * 30 * math.pi / 180) * radius)
         ascii_canvas.add_text(x1, y1, '%s' % mark)
@@ -81,11 +81,11 @@ def draw_clock(cols, lines):
     hour_hand_char = 'O'
     mark_char = '`'
     if os.name == 'nt':
-        single_line_border_chars = ('\xDA', '\xC4', '\xBF', '\xB3', '\x20', '\xB3', '\xC0', '\xC4', '\xD9')
-        second_hand_char = '\xFA'
-        minute_hand_char = '\xF9'
-        hour_hand_char = 'o'
-        mark_char = '\xF9'
+        single_line_border_chars = ('.', '-', '.', '|', ' ', '|', '`', '-', "'")  # ('\xDA', '\xC4', '\xBF', '\xB3', '\x20', '\xB3', '\xC0', '\xC4', '\xD9')
+        second_hand_char = '.'  # '\xFA'
+        minute_hand_char = 'o'  # '\xF9'
+        hour_hand_char = 'O'  # 'o'
+        mark_char = '`'  # '\xF9'
     # create ascii canvas for clock and eval vars
     ascii_canvas = AsciiCanvas(cols, lines)
     center_x = int(math.ceil(cols / 2.0))
@@ -101,10 +101,10 @@ def draw_clock(cols, lines):
     # add regions with weekday and day if possible
     if center_x > 25:
         left_pos = int(radius * x_scale_ratio) / 2 - 4
-        ascii_canvas.add_nine_patch_rect(center_x + left_pos, center_y - 1, 5, 3, single_line_border_chars)
-        ascii_canvas.add_text(center_x + left_pos + 1, center_y, now.strftime('%a'))
-        ascii_canvas.add_nine_patch_rect(center_x + left_pos + 5, center_y - 1, 4, 3, single_line_border_chars)
-        ascii_canvas.add_text(center_x + left_pos + 1 + 5, center_y, now.strftime('%d'))
+        ascii_canvas.add_nine_patch_rect(int(center_x + left_pos), int(center_y - 1), 5, 3, single_line_border_chars)
+        ascii_canvas.add_text(int(center_x + left_pos + 1), int(center_y), now.strftime('%a'))
+        ascii_canvas.add_nine_patch_rect(int(center_x + left_pos + 5), int(center_y - 1), 4, 3, single_line_border_chars)
+        ascii_canvas.add_text(int(center_x + left_pos + 1 + 5), int(center_y), now.strftime('%d'))
     # add clock hands
     draw_second_hand(ascii_canvas, now.second, second_hand_length, fill_char=second_hand_char)
     draw_minute_hand(ascii_canvas, now.minute, minute_hand_length, fill_char=minute_hand_char)
